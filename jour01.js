5be70f85f2fb7291edcf201e7fda788632c5153e
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-07 14:24:22 +0100

    first commit

---
+++ jour01.js
@@ -0,0 +1,38 @@
+console.log("Hello World !");
+
+/* String*/
+
+var test = "My name is Moh";
+console.log(test);
+
+/*03 - Concatenation*/
+
+var name = "Moh"
+console.log("Nice to meet you " + name);
+
+/*04 - String Length*/
+var testLength = "I'm very long !"
+console.log(testLength.length);
+
+/* 05 - Replace */
+
+var food = "croissant is meh";
+console.log(food.replace("meh" , "so good"));
+
+/* 06 - Up and Down */
+var basic = "This is Cool"
+var basicUp = basic.toUpperCase();
+var basicDown = basic.toLowerCase();
+console.log(basic, basicUp, basicDown)
+
+/* 07 - Split */
+var word = "banana"
+var letters = word.split();
+console.log(letters);
+
+/* 08 - Template */
+var age = "30";
+var template = `I'm ${age} years old`;
+console.log(template);
+
+/* ⭐ Bonus */
