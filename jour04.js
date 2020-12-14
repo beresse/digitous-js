550c18e354619f024f582384baab71272ad0eb10
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-14 14:57:56 +0100

    jour 4

---
+++ jour04.js
@@ -0,0 +1,27 @@
+// 01 - Object
+var cat = {
+    name: "Garfiled",
+    age: 3,
+    isCute: true,
+    
+}
+console.log(cat.age)
+if (cat.isCute = true){
+    console.log("So Cute!")
+}
+
+// 02 - Combine
+var cat2 = {
+    name: "Elon",
+    age: 34,
+    isCute: false,
+    
+}
+var cats = [cat,cat2]
+console.log(cats)
+console.log(`L'age de cat est de ${cat.age} ans.`, ` Et pour la beauté de cat 2 c'est ${cat2.isCute}`)
+
+// 03 - Even
+function checkIfEven{
+    num
+}
