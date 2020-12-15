b07e5d16e8a66ba2649b559a36f4a70d880fb0ea
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-15 16:10:06 +0100

    c'est mieux comme cela avec les bons arguments

--- jour05-exo1.js
+++ jour05-exo1.js
@@ -1,22 +1,22 @@
 function calculate(number1, operateur, number2) {
     var number1 = parseInt(number1);
     var number2 = parseInt(number2);
-    if (operateur !== "+") {
+    if (operateur === "+") {
         console.log("Ceci est une addition")
         return number1 + number2
         
-    }else if (operateur !== "-") {
+    }else if (operateur === "-") {
         console.log("Ceci est une soustraction")
         return number1 - number2
         
-    }else if (operateur !== "/") {
+    }else if (operateur === "/") {
         console.log("Ceci est une division")
         return number1 / number2
-    }else if (operateur !== "*") {
+    }else if (operateur === "*") {
         console.log("Ceci est une multiplication")
         return number1 * number2
         
-    }else if (operateur !== "%") {
+    }else if (operateur === "%") {
         console.log("Ceci est un pourcentage*")
         return number1 % number2
         
