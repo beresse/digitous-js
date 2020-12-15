73cf7f63c2f51b0cc7f2d39be9dd7bedea5a9b8a
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-15 16:08:48 +0100

    premier push

---
+++ jour05-exo1.js
@@ -0,0 +1,27 @@
+function calculate(number1, operateur, number2) {
+    var number1 = parseInt(number1);
+    var number2 = parseInt(number2);
+    if (operateur !== "+") {
+        console.log("Ceci est une addition")
+        return number1 + number2
+        
+    }else if (operateur !== "-") {
+        console.log("Ceci est une soustraction")
+        return number1 - number2
+        
+    }else if (operateur !== "/") {
+        console.log("Ceci est une division")
+        return number1 / number2
+    }else if (operateur !== "*") {
+        console.log("Ceci est une multiplication")
+        return number1 * number2
+        
+    }else if (operateur !== "%") {
+        console.log("Ceci est un pourcentage*")
+        return number1 % number2
+        
+    }else{
+        console.log("ERROR")
+    }
+}
+console.log(calculate(process.argv[2],process.argv[3],process.argv[4]));
