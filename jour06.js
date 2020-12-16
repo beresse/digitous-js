da76f339e66b5a165bd0e8a34d4065aa4696240f
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-16 16:12:00 +0100

    Exercice 2 age

--- jour06.js
+++ jour06.js
@@ -7,9 +7,29 @@ console.log(today.formatDate) */
 function formatDate(aaaa,mm,jj){
     var today = new Date();
     var aaaa = today.getFullYear();
-    var mm = today.getMonth();
+    var mm = today.getMonth() + 1;
     var jj = today.getDate()
-    console.log(`${jj} / ${mm} / ${aaaa}`)
+    console.log(`Nous sommes le ${jj} / ${mm} / ${aaaa}`)
 
 }
 formatDate();
+
+// 02
+function calculateAge(aaaa,mm,jj){
+    var today = new Date();
+    var birthday = new Date(aaaa,mm,jj)
+    var age = today.getFullYear() - birthday.getFullYear();
+    var aaaa = birthday.getFullYear();
+    var mm = birthday.getMonth() + 1;
+    var jj = birthday.getDate();
+    console.log(`Vous avez ${age} ans`)
+    
+}
+/*if (calculateAge.length < 3 ) {
+    console.log("Non")
+  
+    
+    Message de condition ne fonctionnant pas
+}*/
+/*console.log("je suis ici")*/
+calculateAge(1990,01,04)
