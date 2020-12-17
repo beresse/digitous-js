21c87b37126b70db9ab326fda1029fd081fca2ed
el.beress@gmail.com <el.beressa@gmail.com>
2020-12-17 13:53:25 +0100

    initial push day 7

---
+++ jour07.js
@@ -0,0 +1,18 @@
+// 01 - Alphabet
+
+function sortLetters(string) {
+
+    var tableau = string.split("");
+    var tableauRetourne = tableau.sort();
+    var sansTableau = tableauRetourne.join()
+    return  sansTableau
+}
+var string = "konexio"
+
+console.log(sortLetters(string))
+
+// 02 - XOXO
+function countEach(string){
+    var numX = 0;
+    var numO = 0;
+}
