aaa52afbe8f69f712f4cb5616d2c41285ac77149
el.beress@gmail.com <el.beressa@gmail.com>
2021-01-05 16:02:41 +0100

    Exercice 1 en paufinement, les exercices 02 et 03 sont terminés

--- jour08.js
+++ jour08.js
@@ -1,12 +1,27 @@
 
-// 01
-var request = require("request")
-var countriesNames = []
-
-request.get("https://restcountries.eu/rest/v1/all", function(error,response,body){
+// 01 Pas terminé de façon propre
+var request = require("request");
+request.get("https://restcountries.eu/rest/v2/all", function (err, res, body) {
+    var countriesName = [""]
     var getCountries = JSON.parse(body)
-    while (i < getCountries.length){
+    console.log(getCountries[1].name)
+});
 
-    }
-    console.log((getCountries[0].name))
-}) 
+// 02 Chuck norris
+request.get("https://api.chucknorris.io/jokes/random", function(error,response,body){
+    console.log(response.statusCode)
+    var getFact = JSON.parse(body);
+    console.log(getFact.value)
+})
+
+// 03 Pokemon  
+function catchPokemon(id){
+    request.get(`https://pokeapi.co/api/v2/pokemon/${id}`, function(error,response,body){
+        
+        var pokemon = JSON.parse(body)
+        console.log(response.statusCode)
+        console.log(`Le pokemon numéro ${id} se nomme ${pokemon.name} en anglais`)
+    })
+}
+
+catchPokemon(4)
